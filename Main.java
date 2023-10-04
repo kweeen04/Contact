6c8c055d68519dfcdd69371d8bcdbc7419865fbf
kweeen04 <thaihongtran1707@gmail.com>
2023-10-04 10:32:15 +0700

    final

---
+++ Main.java
@@ -0,0 +1,12 @@
+import controller.ContactController;
+import controller.ContactManager;
+import view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        ContactController controller = new ContactController();
+        View view = new View();
+        ContactManager manager = new ContactManager(controller, view);
+        manager.run();
+    }
+}
